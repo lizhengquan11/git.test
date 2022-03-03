d3ad7232a925e165e618310444bb95d072200531
HP <lizhnegquan11@126.com>
2022-03-03 15:03:12 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.printf("hello git");
         System.out.printf("hello git2");
+        System.out.printf("hello git3");
+        System.out.printf("hello git4");
     }
 }
